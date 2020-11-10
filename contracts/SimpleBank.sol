be218083d6ea875b2741c9f524829566f0946bef
Graeme <graemebarnes1987@gmail.com>
2020-11-10 07:49:20 +0100

    Update SimpleBank.sol
    Updated function -> fallback

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -50,7 +50,7 @@ contract SimpleBank {
     // Typically, called when invalid data is sent
     // Added so ether sent to this contract is reverted if the contract fails
     // otherwise, the sender's money is transferred to contract
-    function () external payable {
+    fallback () external payable {
         revert();
     }
 
